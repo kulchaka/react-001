3b94cf5c1ed2c54eee5f83926cf0d9b061ad1fca
kulchaka <kulchaka@gmail.com>
2022-02-20 18:28:32 +0100

    add logic and styles fot button and list

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,23 +15,43 @@ class Landing extends Component {
     })
   }
 
+  getDataList = (e) => {
+    this.setState({
+      ...this.state,
+      title: e.title,
+      url: e.url,
+      display: false
+    })
+  }
+
+  chooseRestaurant =() => {
+    console.log('You have chosen restaurant>>>: ', this.state.title)
+  }
+
   render() {
-    // console.log(this)
     return (
       <div className='restaurant_select'>
         <div onClick={this.displayList} className='restaurant_select_top'>
-          <div className='restaurant_select_top-header font-effect-outline'>Select Restaurant</div>
+          <div
+            className='restaurant_select_top-header font-effect-outline'>
+            {
+              !this.state.title
+                ? 'Select Restaurant'
+                : `${this.state.title}`
+            }
+          </div>
           <div className='arrow_picker'>
             <div className='arrow_picker-up'></div>
             <div className='arrow_picker-down'></div>
           </div>
         </div>
         {
-          this.state.display ?
+          this.state.display
+            ?
             (<div className='restaurant_select_bottom'>
               <ul>
                 {
-                  restaurants.map(e => <li key={e.id}>{e.title}</li>)
+                  restaurants.map(e => <li onClick={() => this.getDataList(e)} key={e.id}>{e.title}</li>)
                 }
               </ul>
             </div>)
@@ -40,7 +60,11 @@ class Landing extends Component {
 
         }
 
-        <button>Enter</button>
+        {
+          !this.state.display && this.state.title
+            ? <button onClick={this.chooseRestaurant}>Enter</button>
+            : null
+        }
       </div>
     );
   }
