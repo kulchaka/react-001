f48fa60dca8f02091e96af8e7486e6f520359745
kulchaka <kulchaka@gmail.com>
2022-02-20 18:01:05 +0100

    add state and work with list

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,23 +2,44 @@ import React, {Component} from 'react';
 import restaurants from "../sample-restaurants";
 
 class Landing extends Component {
+  state = {
+    display: false,
+    title: '',
+    url: ''
+  }
+
+  displayList = () => {
+    this.setState({
+      ...this.state,
+      display: !this.state.display
+    })
+  }
+
   render() {
+    // console.log(this)
     return (
       <div className='restaurant_select'>
-        <div className='restaurant_select_top'>
+        <div onClick={this.displayList} className='restaurant_select_top'>
           <div className='restaurant_select_top-header font-effect-outline'>Select Restaurant</div>
           <div className='arrow_picker'>
             <div className='arrow_picker-up'></div>
             <div className='arrow_picker-down'></div>
           </div>
         </div>
-        <div className='restaurant_select'>
-          <ul>
-            {
-              restaurants.map(e => <li key={e.id}>{e.title}</li>)
-            }
-          </ul>
-        </div>
+        {
+          this.state.display ?
+            (<div className='restaurant_select_bottom'>
+              <ul>
+                {
+                  restaurants.map(e => <li key={e.id}>{e.title}</li>)
+                }
+              </ul>
+            </div>)
+            :
+            null
+
+        }
+
         <button>Enter</button>
       </div>
     );
