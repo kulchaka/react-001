b51846291f7146fb69fe4d2b1a9cf6985e541057
kulchaka <kulchaka@gmail.com>
2022-03-13 11:54:58 +0100

    add Refs for inputs

--- src/components/AddBurgerForm.jsx
+++ src/components/AddBurgerForm.jsx
@@ -1,36 +1,51 @@
-import React, {Component} from 'react';
+import React, {Component, createRef} from 'react';
 
 class AddBurgerForm extends Component {
+
+  nameRef = createRef()
+  priceRef = createRef()
+  statusRef = createRef()
+  descrRef = createRef()
+  imageRef = createRef()
 
   createBurger = (event) => {
     event.preventDefault()
     console.log("create Burger")
+    console.log(this.nameRef.current.value)
+    console.log(this.priceRef.current.value)
+    console.log(this.statusRef.current.value)
+    console.log(this.descrRef.current.value)
+    console.log(this.imageRef.current.value)
   }
 
   render() {
     return (
       <form className="burger-edit" onSubmit={this.createBurger}>
         <input
+          ref={this.nameRef}
           type="text"
           name="name"
           placeholder="Name"
           autoComplete="off"
         />
         <input
+          ref={this.priceRef}
           type="text"
           name="price"
           placeholder="Price"
           autoComplete="off"
         />
         <select
+          ref={this.statusRef}
           name="status"
           className="status"
         >
           <option value="available">Accessed</option>
           <option value="unavailable">NOT available</option>
         </select>
-        <textarea name="decr" placeholder="Desc"/>
+        <textarea ref={this.descrRef} name="descr" placeholder="Descr"/>
         <input
+          ref={this.imageRef}
           type="text"
           name="image"
           placeholder="Image"
