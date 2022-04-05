a3a1dd3828ba2fd383f15519318f5e6457ea696a
kulchaka <kulchaka@gmail.com>
2022-04-05 21:07:33 +0200

    sync FireBase with APP

---
+++ src/base.js
@@ -0,0 +1,18 @@
+import Rebase from 're-base'
+import firebase from "firebase";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyAR_EHaj5sKqv8xfZLMvNtXQ_HZFw0ms3Y",
+  authDomain: "burger-store-hot.firebaseapp.com",
+  databaseURL: "https://burger-store-hot-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "burger-store-hot",
+  storageBucket: "burger-store-hot.appspot.com",
+  messagingSenderId: "855328216283",
+  appId: "1:855328216283:web:9543a231770d51e220a8fe"
+})
+
+const base = Rebase.createClass(firebaseConfig.database())
+
+export {firebaseConfig}
+
+export default base
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,26 @@ import Order from "./Order";
 import MenuManagement from "./MenuManagement";
 import sampleBurgers from "../sample-burgers";
 import Burger from "./Burger";
+import base from "../base";
 
 class App extends Component {
 
   state = {
     burgers: {},
     orders: {}
+  }
+
+  componentDidMount() {
+    this.ref = base.syncState(`${this.props.match.params.restaurantId}/burgers`,
+      {
+        context: this,
+        state: 'burgers'
+      }
+      )
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref)
   }
 
   addBurger = (burger) => {
