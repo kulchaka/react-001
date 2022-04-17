77992038dcd1d11ffbc7087387c3bec87adc5897
kulchaka <kulchaka@gmail.com>
2022-04-17 20:58:41 +0200

    component Burger use Hooks

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,50 +1,47 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
-class Burger extends Component {
+const Burger = (props) => {
 
-  static propTypes = {
-    addToOrder: PropTypes.func,
-    index: PropTypes.string,
-    details: PropTypes.shape({
-      desc: PropTypes.string,
-      image: PropTypes.string,
-      name: PropTypes.string,
-      price: PropTypes.number,
-      status: PropTypes.string
-    })
+  const  handleClick = () => {
+    props.addToOrder(props.index)
   }
 
-  handleClick = () => {
-    this.props.addToOrder(this.props.index)
-  }
+  const {name, price, image, desc, status} = props.details
+  const isAvailable = status === 'available'
 
-  render() {
+  return (
+    <li className="menu-burger">
+      <div className="image-container">
+        <img src={image} alt={name}/>
+      </div>
+      <div className="burger-details">
+        <h3 className="burger-name">
+          {name}
+          <span className="price">{price}</span>
+        </h3>
+        <p>{desc}</p>
+        <button
+          onClick={handleClick}
+          disabled={!isAvailable}
+          className="buttonOrder">
+          {isAvailable ? 'Order' : 'NOT Available'}
+        </button>
+      </div>
+    </li>
+  );
+};
 
-    const {name, price, image, desc, status} = this.props.details
-    const isAvailable = status === 'available'
-
-    return (
-      <li className="menu-burger">
-        <div className="image-container">
-          <img src={image} alt={name}/>
-        </div>
-        <div className="burger-details">
-          <h3 className="burger-name">
-            {name}
-            <span className="price">{price}</span>
-          </h3>
-          <p>{desc}</p>
-          <button
-            onClick={this.handleClick}
-            disabled={!isAvailable}
-            className="buttonOrder">
-            {isAvailable ? 'Order' : 'NOT Available'}
-          </button>
-        </div>
-      </li>
-    );
-  }
+Burger.propTypes = {
+  addToOrder: PropTypes.func,
+  index: PropTypes.string,
+  details: PropTypes.shape({
+    desc: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    price: PropTypes.number,
+    status: PropTypes.string
+  })
 }
 
 export default Burger;
