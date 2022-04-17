cfee26158bd5eab38a9a1cb9998d862f27c77234
kulchaka <kulchaka@gmail.com>
2022-04-17 20:54:28 +0200

    component Shipment use Hooks

--- src/components/MenuManagement.jsx
+++ src/components/MenuManagement.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AddBurgerForm from "./AddBurgerForm";
 import EditBurgerForm from "./EditBurgerForm";
 import PropTypes from "prop-types";
--- src/components/Shipment.jsx
+++ src/components/Shipment.jsx
@@ -1,36 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
-class Shipment extends Component {
+const Shipment = (props) => {
 
-  static propTypes = {
-    totalPrice: PropTypes.number
-  }
-
-  render() {
-    const shipping = this.props.totalPrice > 0 && this.props.totalPrice < 500 ? 99 : 0
-    const shippingNeon = shipping === 0 ? (
+  const shipping = props.totalPrice > 0 && props.totalPrice < 500 ? 99 : 0
+  const shippingNeon = shipping === 0 ? (
       <span className="font-effect-neon total_wrap-cheap">FREE</span>
     )
-      : (
-        <span>{shipping}</span>
-      )
-    return (
-      <div className="total">
-        <div className="total_wrap">
-          <div>Shipping: {shippingNeon}</div>
-          <div className="total_wrap-free">
-            {
-              this.props.totalPrice < 500
-                ? `Please order ${500 - this.props.totalPrice} more for free shipping`
-                : null
-            }
-          </div>
-          <div className="total_wrap-final">Total price: {this.props.totalPrice}</div>
+    : (
+      <span>{shipping}</span>
+    )
+
+  return (
+    <div className="total">
+      <div className="total_wrap">
+        <div>Shipping: {shippingNeon}</div>
+        <div className="total_wrap-free">
+          {
+            props.totalPrice < 500
+              ? `Please order ${500 - props.totalPrice} more for free shipping`
+              : null
+          }
         </div>
+        <div className="total_wrap-final">Total price: {props.totalPrice}</div>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+Shipment.propTypes = {
+  totalPrice: PropTypes.number
 }
 
 export default Shipment;
