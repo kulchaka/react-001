2321b878d57115fc4b1e1a779212587b9c7043eb
kulchaka <kulchaka@gmail.com>
2022-04-17 20:51:29 +0200

    component Order use Hooks

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,32 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Shipment from "./Shipment";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 import PropTypes from "prop-types";
 
-class Order extends Component {
+const Order = (props) => {
 
-  static propTypes = {
-    burgers: PropTypes.object,
-    orders: PropTypes.object,
-    deleteBurgerFromOrder: PropTypes.func
-  }
+  const renderList = (key) => {
+    const isAvailable = props.burgers[key] && props.burgers[key].status === 'available'
 
-  render() {
-    const renderList = (key) => {
-      const isAvailable = this.props.burgers[key] && this.props.burgers[key].status === 'available'
+    if (!props.burgers[key]) return null
 
-      if (!this.props.burgers[key]) return null
-
-      if (!isAvailable) {
-        return (<CSSTransition
-          classNames="order"
-          key={key}
-          timeout={{enter: 500, exit: 500}}
-        >
-          <li className="unavailable" key={key}> Sorry, {this.props.burgers[key].name} NOT AVAILABLE</li>
-        </CSSTransition>)
-      }
+    if (!isAvailable) {
       return (<CSSTransition
+        classNames="order"
+        key={key}
+        timeout={{enter: 500, exit: 500}}
+      >
+        <li className="unavailable" key={key}> Sorry, {props.burgers[key].name} NOT AVAILABLE</li>
+      </CSSTransition>)
+    }
+    return (
+      <CSSTransition
         classNames="order"
         key={key}
         timeout={{enter: 500, exit: 500}}
@@ -36,36 +30,43 @@ class Order extends Component {
           <TransitionGroup component='span' className='count'>
             <CSSTransition
               classNames='count'
-              key={this.props.orders[key]}
+              key={props.orders[key]}
               timeout={{enter: 500, exit: 500}}
             >
-              <span>{this.props.orders[key]} </span>
+              <span>{props.orders[key]} </span>
               </CSSTransition>
             </TransitionGroup>
-           x burgers {this.props.burgers[key].name}
-          <span> {this.props.orders[key] * this.props.burgers[key].price}</span>
-          <button onClick={() => this.props.deleteBurgerFromOrder(key)} className="cancellItem">&times;</button>
+           x burgers {props.burgers[key].name}
+          <span> {props.orders[key] * props.burgers[key].price}</span>
+          <button onClick={() => props.deleteBurgerFromOrder(key)} className="cancellItem">&times;</button>
         </span>
         </li>
       </CSSTransition>)
+  }
+  const orderIds = Object.keys(props.orders)
+  const totalPrice = orderIds.reduce((prev, key) => {
+    const isAvailable = props.burgers[key] && props.burgers[key].status === 'available'
+    if (isAvailable) {
+      return prev + props.burgers[key].price * props.orders[key]
     }
+    return prev
+  }, 0)
 
-    const orderIds = Object.keys(this.props.orders)
-    const totalPrice = orderIds.reduce((prev, key) => {
-      const isAvailable = this.props.burgers[key] && this.props.burgers[key].status === 'available'
-      if (isAvailable) {
-        return prev + this.props.burgers[key].price * this.props.orders[key]
-      }
-      return prev
-    }, 0)
-    return (<div className='order-wrap'>
+  return (
+    <div className='order-wrap'>
       <h2>Your order:</h2>
       <TransitionGroup component="ul" className="order">
         {orderIds.map(e => renderList(e))}
       </TransitionGroup>
       {totalPrice ? (<Shipment totalPrice={totalPrice}/>) : (<div className="nothingSelected">Nothing Selected</div>)}
-    </div>);
-  }
+    </div>
+  );
+};
+
+Order.propTypes = {
+  burgers: PropTypes.object,
+  orders: PropTypes.object,
+  deleteBurgerFromOrder: PropTypes.func
 }
 
 export default Order;
